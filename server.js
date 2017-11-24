540bf084d15e8019db17c75e9e0afaac0cea8cb1
Dave Nicholas <davenicholasuk@gmail.com>
2017-11-24 18:46:03 +0000

    Add running server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import schema from './src/schema';
 
-//const myGraphQLSchema = // ... define or import your schema here!
 const PORT = 3000;
 
 const app = express();
 
-// bodyParser is needed just for POST.
-//app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
-//app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 app.listen(PORT);
---
+++ src/schema.js
@@ -0,0 +1,23 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+const typeDefs = `
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
+type Query {
+  author(firstName: String, lastName: String): Author
+  getFortuneCookie: String
+}
+`;
+
+export default makeExecutableSchema({ typeDefs });
