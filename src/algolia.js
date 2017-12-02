fdd078a2c64ed7567811ce2e8e0230e43dec316c
Dave Nicholas <davenicholasuk@gmail.com>
2017-12-02 01:44:45 +0000

    Change remove log

--- src/algolia.js
+++ src/algolia.js
@@ -2,8 +2,6 @@ import Algolia from 'algoliasearch';
 import { config } from 'dotenv';
 config();
 
-console.log('qwerqwerq', process.env.ALGOLIA_APP_ID);
-
 const client = Algolia(
   process.env.ALGOLIA_APP_ID,
   process.env.ALGOLIA_ADMIN_API_KEY
